3577d1695a85c03d86345f1cc95fb2c96c6a3f95
Bartek <bartekbednarek51@gmail.com>
2020-06-03 15:28:57 +0200

    ładowanie faktury z dysku na serwer i z serwera na strone

--- path.py
+++ path.py
@@ -1,2 +1,5 @@
 def getPath():
     return "E:/wazne rzeczy/SEMESTR 10 FINALL BOSS/tesseract"
+
+def getUploadFiles():
+    return "C:/Users/Bartek/PycharmProjects/magisterka/static"
--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request, redirect
 import os
+import  path
 
 app = Flask(__name__)
+app.config["IMAGE_UPLOADS"] = path.getUploadFiles()
+UPLOADS = os.path.join("static", "uploads")
+app.config["UPLOAD_FOLDER"] = UPLOADS
+
 
 @app.route("/")
 def main():
@@ -12,11 +17,12 @@ def getInvoice():
     if request.method == "POST":
         if request.files:
             invoice = request.files["invoice"]
-            print(invoice)
-            return redirect(request.url)
+            invoice.save(os.path.join(app.config["IMAGE_UPLOADS"], invoice.filename))
+            #print(invoice)
+            return render_template('index.html', invoiceName=invoice.filename)
+            #return redirect(request.url)
 
     return render_template('index.html')
-
 
 if __name__ == "__main__":
     app.run(debug = True)
